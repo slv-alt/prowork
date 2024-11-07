7d77799be044066543912ad107721865a739388b
root <slv-alt@mail.ru>
2024-11-07 23:32:56 +0400

    mod bootstrap.sh

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 cd ~
+rm -r kubespray
 git clone https://github.com/slv-alt/kubespray.git
 cd kubespray
 ansible-playbook -i inventory/mycluster/hosts.yaml --become --become-user=root cluster.yml
