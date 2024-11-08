fdb2b19adf40ba00e793d5103dd86fdff5012018
root <slv-alt@mail.ru>
2024-11-08 15:40:52 +0400

    mod bootstrap.sh

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-cd ~
-rm -r kubespray
+kubespray_home_dir="/root"
+cd ${kubespray_home_dir}
+if ! [ -d ${kubespray_home_dir/}kubespray ]; then
 git clone https://github.com/slv-alt/kubespray.git
-cd kubespray
+fi
+cd ${kubespray_home_dir}/kubespray
 ansible-playbook -i inventory/mycluster/hosts.yaml --become --become-user=root cluster.yml
