3707cd63dba1a1e446a77f33d5e78d964f428662
root <slv-alt@mail.ru>
2024-11-10 18:01:40 +0400

    mod deploy-infra.sh

--- deploy-infra/deploy-infra.sh
+++ deploy-infra/deploy-infra.sh
@@ -14,6 +14,7 @@ kubectl apply -f minio.yaml
 # Создаем корзины: chunks,ruler,admin
 # Пользователя с правами RW: loki-user:12345678
 
+sleep 60
 
 # Создадим пространство имен для мониторига
 kubectl apply -f ns-monitoring.yaml
@@ -24,13 +25,18 @@ helm repo add grafana https://grafana.github.io/helm-charts
 # Устанавливаем Loki
 helm install loki grafana/loki --values val-loki.yaml -n monitoring
 
+sleep 60
+
 # Устанавливаем Promtail
 helm install promtail grafana/promtail --values val-prom.yaml -n monitoring
+
+sleep 60
 
 # Устанавлимаем стек мониторинга
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm install kube-prom-stack prometheus-community/kube-prometheus-stack --values val-kub-prom-stack.yaml -n monitoring
 
+sleep 60
 
 ### ПО для деплоя
 # Argo
@@ -42,4 +48,4 @@ helm repo add argo https://argoproj.github.io/argo-helm
 helm install argo-cd argo/argo-cd --values val-argo-cd.yaml --namespace argo --create-namespace
 
 # Пароль для веб-интерфейса, пользователь admin
-# kubectl -n argo get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
+kubectl -n argo get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
